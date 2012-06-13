e79de0162760715ea4165cbf5c27e172116dc645
Shahar Mintz <shahar.mintz309@gmail.com>
2012-06-13 14:43:49 +0300

    move passwords and users configuration to params.pp

--- manifests/params.pp
+++ manifests/params.pp
@@ -17,4 +17,13 @@ class mysql_mmm::params {
   $writer_ip =  [ "192.168.99.250" ]
   $readers = $writers
   $readers_ips = [ "192.168.99.251" ]
+
+  $agent_user = "mmm_agent"
+  $agent_password = "agent_password"
+
+  $replication_user = "replication"
+  $replication_password = "replication_password"
+
+  $monitor_user = "mmm_monitor"
+  $monitor_password = "monitor_password"
 }
